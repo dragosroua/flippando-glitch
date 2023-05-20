382cd84d9d84dfa6b670b9797932ce4d43748551
Dragos Roua <dragosroua@gmail.com>
2023-05-20 05:53:47 -0400

    new deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
     module.exports = {
-      svgAddress: "0x9187a4CDF07dD85Bb256CC918f01466B4bee8313",
-      flippandoAddress: "0x8F0408A1a37FA79E789E155dD87aD46bdcE87367",
-      flipAddress: "0xf4d7673e8550D5ccE795198A37b32BEF84bE56F8",
-      flippandoBundlerAddress: "0x40Ce7fA7Ec233C51624D8280f2FeD32aD3df9662",
+      svgAddress: "0x9b93BBBA15bcb2094AED645774D7E3aFCF74f340",
+      flippandoAddress: "0x968459dCCD2762A0E5c5F0EE3ca2dBfb567fbd57",
+      flipAddress: "0xF3f6a994b982f7F5E67fDc0ef5ad248D7fFa6E7e",
+      flippandoBundlerAddress: "0x786498f8d7936320B6ce1f2577dB9D75E9529DD1",
     }
